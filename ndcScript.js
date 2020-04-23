e92ddd534b2e68741ca37df4a6a47725809774bb
Hallom <challom@pharmalogistics.com>
2020-04-23 15:20:49 -0500

    Added different URL for the NDC v. label.

--- ndcScript.js
+++ ndcScript.js
@@ -1,5 +1,6 @@
 var NDC = document.getElementById("NDC");
 
-var mainURL = "https://api.fda.gov/drug/ndc.json?search="; // The base URL.
-var package_ndc = "openfda.package_ndc:"; // This is the addon to the URL if the lookup is for an NDC.
-var product_ndc = "openfda.product_ndc"; // addon for product NDC lookup. 
+var mainURL = "https://api.fda.gov/drug/label.json?search="; // The base URL.
+var ndcURL = " https://api.fda.gov/drug/ndc.json?search="; // The base URL for NDC searach
+var Label_package_ndc = "openfda.package_ndc:"; // This is the addon to the URL if the lookup is for an NDC.
+var Label_product_ndc = "openfda.product_ndc:"; // addon for product NDC lookup. 
