738fa15b3dba369f4bf88001c07dd92bb761a0ff
Hallom <challom@pharmalogistics.com>
2020-04-29 20:03:37 -0500

    Clearing whitespace.

--- ndcScript.js
+++ ndcScript.js
@@ -8,45 +8,44 @@ var Label_package_ndc = "openfda.package_ndc:"; // This is the addon to the URL 
 var Label_product_ndc = "openfda.product_ndc.exact:"; // addon for product NDC lookup. 
 var ndc1 = document.getElementById("ndc1");
 var ndc2 = document.getElementById("ndc2");
+
 function findNDC(){
    fetch(mainURL+ Label_package_ndc+"\""+NDC.value+"\"")
     .then((response) =>{
         return response.json();
     })
     .then((data) =>  {
-        
-        
-      
+        // When I looped through the JSON, I kept receiving "undefined" errors. 
+        // I sent the data to another function to loop through and everything works. 
        showData(data)
-     // console.log(data.results[i].openfda)
-        
     });   
 }
+
 function showData(data){
 
    for(var i = 0; i< data.results.length; i++){
-       
-    console.log(data.results[i].openfda.brand_name)
+    // Loops through the json to fill in the fields. 
+    console.log(data.results[i])
     productName.value = data.results[i].openfda.brand_name.toString()
     productManu.value = data.results[i].openfda.manufacturer_name.toString()
+    // If the first package NDC field is empty, there won't be a second NDC. 
     if( data.results[0].openfda.package_ndc[0] == null ){
         ndc1.value = ""; 
         ndc2.value = "";
 
     }
-    else if(data.results[0].openfda.package_ndc[1] == null){
+    // If the second one is null, then it will fill the first and clear the second field. 
+    else if(data.results[0].openfda.package_ndc[0] != null & data.results[0].openfda.package_ndc[1] == null ){
         ndc1.value = data.results[0].openfda.package_ndc[0].toString();
+        ndc2.value = ""; 
     }
     else{
+        // If both are present. Fill in both. 
         ndc1.value = data.results[0].openfda.package_ndc[0].toString();
         ndc2.value = data.results[0].openfda.package_ndc[1].toString();
-    }
-   
-    
-    
+        }
    }
    
-    
 }
 
 function ClearAll(){
