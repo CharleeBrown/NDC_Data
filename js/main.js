9a93e352ec76611d9a6ed06e4b7247540542739b
Coleman Hallom <challom@pharmalogistics.com>
2020-05-15 13:37:20 -0500

    Serving webpage via node only. no express.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
 const http = require("http");
 const hostname = "127.0.0.1";
 const port = 8200;
+const url = require("url");
+const fs = require("fs");
 
-// Create HTTP Server
+
+// Tutorial via https://www.c-sharpcorner.com/article/creating-server-and-host-html-page-using-node-js/
 const server = http.createServer((req, res) => {
-    //Set the response HTTP header with HTTP status and Content
-    res.writeHead(200, {'Content-Type':'text/plain'});
+    const path = url.parse(request.url).pathname;
+    switch(path){
+        case '/':
+                res.writeHead(200, {'Content-Type':'text/plain'});
+                res.end('What up tho');  
+                break;
+        case '/main.html':
+                fs.readFile(_dirname + path, function(err, data){
+                if(err){
+                    res.writeHead(404);
+                    res.write(error);
+                    res.end();
 
-    // Send the response body "Hello World"
-    res.end('What up tho');
-
-
+                         }   
+                else {
+                    res.writeHead(200, {'Content-Type':'text/plain'});
+                    res.write(data);
+                    res.end();
+                        }
+                });
+            break;
+        }
 });
 
 // Prints a log to the console once the server starts listening. 
     server.listen(port, hostname,() => {
         console.log('Server running at http://${hostname}:${port}/')
 
-    })
+    });
