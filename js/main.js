73e2f5dd7a4a3644567e5e03240fcafa8d8ce04d
Coleman Hallom <challom@pharmalogistics.com>
2020-05-15 16:45:00 -0500

    Added formatting and express script.

--- js/main.js
+++ js/main.js
@@ -8,31 +8,34 @@ const fs = require("fs");
 // Tutorial via https://www.c-sharpcorner.com/article/creating-server-and-host-html-page-using-node-js/
 const server = http.createServer((req, res) => {
     const path = url.parse(request.url).pathname;
-    switch(path){
+    switch (path) {
         case '/':
-                res.writeHead(200, {'Content-Type':'text/plain'});
-                res.end('What up tho');  
-                break;
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('What up tho');
+            break;
         case '/main.html':
-                fs.readFile(_dirname + path, function(err, data){
-                if(err){
+            fs.readFile(_dirname + path, function (err, data) {
+                if (err) {
                     res.writeHead(404);
                     res.write(error);
                     res.end();
 
-                         }   
-                else {
-                    res.writeHead(200, {'Content-Type':'text/plain'});
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/plain'
+                    });
                     res.write(data);
                     res.end();
-                        }
-                });
+                }
+            });
             break;
-        }
+    }
 });
 
 // Prints a log to the console once the server starts listening. 
-    server.listen(port, hostname,() => {
-        console.log('Server running at http://${hostname}:${port}/')
+server.listen(port, hostname, () => {
+    console.log('Server running at http://${hostname}:${port}/')
 
-    });
+});
--- js/ndcScript.js
+++ js/ndcScript.js
@@ -2,8 +2,10 @@
 var NDC = document.getElementById("NDC");
 var productName = document.getElementById("prodName");
 var productManu = document.getElementById("prodManu");
+
 var mainURL = "https://api.fda.gov/drug/label.json?search="; // The base URL.
 var ndcURL = " https://api.fda.gov/drug/ndc.json?search="; // The base URL for NDC searach
+
 var Label_package_ndc = "openfda.package_ndc:"; // This is the addon to the URL if the lookup is for an NDC.
 var Label_product_ndc = "openfda.product_ndc.exact:"; // addon for product NDC lookup. 
 var ndc1 = document.getElementById("ndc1");
